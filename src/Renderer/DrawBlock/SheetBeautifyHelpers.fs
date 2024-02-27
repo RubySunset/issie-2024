0b5c1d28b3b4925e119c1043f2344330fd59fefb
elkouny <108074024+elkouny@users.noreply.github.com>
2024-02-27 18:49:22 +0000

    Fixed Some Functions Up to T2 done. Lens Cleanup needed

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -2,55 +2,47 @@
 
 open CommonTypes
 open DrawModelType
-open Symbol
 
+open Symbol
+open BlockHelpers
 
 //-----------------Module for beautify Helper functions--------------------------//
 // Typical candidates: all individual code library functions.
 // Other helpers identified by Team
 module Helpers =
-    let visibleSegments  (model: SheetT.Model) (wId: ConnectionId) : XYPos list =
 
-        let wire = model.Wire.Wires[wId] // get wire from model
+    let getWiresInBox (model: SheetT.Model) (box: BoundingBox)  : (BusWireT.Wire * int) list =
+        let wires = (List.ofSeq (Map.values model.Wire.Wires))
 
-        /// helper to match even and off integers in patterns (active pattern)
-        let (|IsEven|IsOdd|) (n: int) =
-            match n % 2 with
-            | 0 -> IsEven
-            | _ -> IsOdd
+        let bottomRight =
+            {   X = box.TopLeft.X + box.W
+                Y = box.TopLeft.Y + box.H
+            }
 
-        /// Convert seg into its XY Vector (from start to end of segment).
-        /// index must be the index of seg in its containing wire.
-        let getSegmentVector (index: int) (seg: BusWireT.Segment) =
-            // The implicit horizontal or vertical direction  of a segment is determined by
-            // its index in the list of wire segments and the wire initial direction
+        // State Tuple - (overlapping: bool, overlapping_wire_index: int)
+        let checkOverlapFolder (startPos: XYPos) (endPos: XYPos) (state: bool * int) (segment: BusWireT.Segment) : bool * int =
+            let overlap = overlap2D (startPos, endPos) (box.TopLeft, bottomRight)
+            (fst state || overlap), if overlap then segment.Index else snd state
+
+        List.map (fun w -> foldOverNonZeroSegs checkOverlapFolder (false, -1) w, w) wires
+        |> List.filter (fun l -> fst (fst l))
+        |> List.map (fun ((_, index), w) -> w, index)
+
+    let getSegmentVector  (wire:BusWireT.Wire) (index:int)  :XYPos =
+            //Takes Segment index and wire and returns XY representation
+            let (|IsEven|IsOdd|) (n: int) =
+                match n % 2 with
+                | 0 -> IsEven
+                | _ -> IsOdd
+
             match index, wire.InitialOrientation with
-            | IsEven, BusWireT.Vertical
-            | IsOdd, BusWireT.Horizontal -> { X = 0.; Y = seg.Length }
-            | IsEven, BusWireT.Horizontal
-            | IsOdd, BusWireT.Vertical -> { X = seg.Length; Y = 0. }
-
-        /// Return a list of segment vectors with 3 vectors coalesced into one visible equivalent
-        /// if this is possible, otherwise return segVecs unchanged.
-        /// Index must be in range 1..segVecs
-        let tryCoalesceAboutIndex (segVecs: XYPos list) (index: int) =
-            if segVecs[index] =~ XYPos.zero then
-                segVecs[0 .. index - 2]
-                @ [ segVecs[index - 1] + segVecs[index + 1] ]
-                @ segVecs[index + 2 .. segVecs.Length - 1]
-            else
-                segVecs
-
-        wire.Segments
-        |> List.mapi getSegmentVector
-        |> (fun segVecs ->
-            (segVecs, [ 1 .. segVecs.Length - 2 ])
-            ||> List.fold tryCoalesceAboutIndex)
+            | IsEven, BusWireT.Vertical | IsOdd, BusWireT.Horizontal -> {X=0.; Y=wire.Segments[index].Length}
+            | IsEven, BusWireT.Horizontal | IsOdd, BusWireT.Vertical -> {X=wire.Segments[index].Length; Y=0.}
 
 
 module B1 =
-    let B1R (symbol : SymbolT.Symbol) = (symbol.LabelBoundingBox.H,symbol.LabelBoundingBox.W)
-    let B1W (symbol:SymbolT.Symbol) (H:float) (W:float) = {symbol with LabelBoundingBox.H = H ; LabelBoundingBox.W = W }
+    let B1R (symbol : SymbolT.Symbol) = (symbol.Component.H,symbol.Component.W)
+    let B1W (symbol:SymbolT.Symbol) (H:float) (W:float) = {symbol with SymbolT.Symbol.Component.H = H ; SymbolT.Symbol.Component.W =W}
 
 
 module B2 =
@@ -62,10 +54,13 @@ module B3 =
     let B3R (symbol : SymbolT.Symbol) (edge : Edge) = symbol.PortMaps.Order[edge]
 
     let B3W (symbol:SymbolT.Symbol) (edge : Edge) (ports : List<string>) =
-        {symbol with PortMaps.Order = Map [edge,ports] }
+
+        {symbol with PortMaps.Order= Map.add edge ports symbol.PortMaps.Order }
 
 module B4 =
     let B4R (symbol :SymbolT.Symbol) = symbol.ReversedInputPorts
+
+    //TODO:check if i should switch state or just set to false
     let B4W (symbol :SymbolT.Symbol) =
         {symbol with ReversedInputPorts = Some true }
 module B5 =
@@ -73,7 +68,7 @@ module B5 =
          symbol.Pos + getPortPos symbol port
 
 module B6 =
-    let B6R (symbol :SymbolT.Symbol) = symbol.LabelBoundingBox
+    let B6R (symbol :SymbolT.Symbol) = getSymbolBoundingBox symbol
 
 module B7 =
 
@@ -95,23 +90,33 @@ module T1 =
             |> List.mapi (fun i bb -> (i, bb))
 
         List.allPairs boundingBoxIndexed boundingBoxIndexed
-        |> List.filter (fun ((i1,bb1), (i2,bb2)) -> ((BlockHelpers.overlap2DBox bb1 bb2) && i1<>i2))
-        |>List.length
-
-module T2 =
-    open Helpers
-    let T2R (sheet :SheetT.Model) =
-        let cIds = List.ofArray( mapKeys sheet.Wire.Wires)
-        let visibleSegmentXY = List.collect (visibleSegments sheet) cIds
-        let symbolsXY = Map.values sheet.Wire.Symbol.Symbols
-                        |> Seq.toList
-                        |> List.map (fun symbol -> symbol.Pos)
-        List.allPairs symbolsXY visibleSegmentXY
-        |>List.filter (fun (pos1,pos2)->pos1=~pos2)
-        |>List.map snd
+        |> List.filter (fun ((i1,bb1), (i2,bb2)) -> ((overlap2DBox bb1 bb2) && i1<>i2))
+        |>List.map (fun ((i1, bb1), (i2, bb2)) -> if i1 < i2 then (i1, i2) else (i2, i1))
         |>List.distinct
         |>List.length
 
+
+module T2 =
+    open Helpers
+    let T2R (sheet:SheetT.Model) =
+        let boundingBoxes =
+            sheet.BoundingBoxes
+            |> Map.values
+            |> Seq.toList
+
+        boundingBoxes
+        |> List.collect (getWiresInBox sheet)
+        |> List.distinct
+        |> List.map (fun (busWires, idx) -> getSegmentVector busWires idx)
+        |> List.filter (fun pos -> not (pos=~XYPos.zero))
+        |>List.length
+
+
+
+module T3 =
+
+
+    let T3R (sheet :SheetT.Model)=
 
 
 
