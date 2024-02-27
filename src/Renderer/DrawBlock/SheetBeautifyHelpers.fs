26811d2989a31ba2d4e2a45a654fce9012529e2a
Daniel Coroama <daniel.coroama21@imperial.ac.uk>
2024-02-27 12:39:45 +0000

    Progress on helper functions

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -7,14 +7,13 @@
 open DrawModelType.SymbolT
 open CommonTypes
 open Optics
-open SymbolHelpers
 
 // B1R
 let getCustomComponentDimensions (sym: Symbol) : float * float = 
     // Use of lens already provided for getting bounding box
     match sym.Component.Type with
     | Custom _ ->
-        let box = fst labelBoundingBox_ sym
+        let box = sym.LabelBoundingBox
         box.W, box.H
     | _ -> failwithf "Non-custom component pattern matching with get"
 
@@ -25,9 +24,33 @@ let setCustomComponentDimensions
     : Symbol =
     match sym.Component.Type with
     | Custom _ ->
-        let box = fst labelBoundingBox_ sym
+        let box = sym.LabelBoundingBox
         let newBox = {box with W = fst newDim; H = snd newDim}
-        snd labelBoundingBox_ newBox sym
+        snd labelBoundingBox_ newBox sym // Using provided lenses
     | _ -> failwithf "Non-custom component pattern matching with set"
 
+// B1 Lens
 let customComponentDimension_ = Lens.create getCustomComponentDimensions setCustomComponentDimensions
+
+// B2
+let setSymbolPos (sym: Symbol) (pos: XYPos) =
+    {sym with Pos=pos}
+
+// B3R
+// Read port order on specified side of a symbol.
+let getPortOrder (side: Edge) (sym: Symbol) =
+    sym.PortMaps.Order[side]
+
+// B3W
+// Write port order on a specified side of a symbol
+let setPortOrder (side: Edge) (order: string list) (sym: Symbol) =
+    let oldPortOrder = sym.PortMaps.Order
+    let newPortOrder = Map.add side order oldPortOrder
+    {sym with PortMaps={sym.PortMaps with Order=newPortOrder}}
+
+// B3 Lens
+let changePortOrder_ (edge: Edge) = Lens.create (getPortOrder edge) (setPortOrder edge)
+
+// B4R -- Don't understand requirement
+
+// B5R
