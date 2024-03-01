16d90c060af58945103b93943fbf1bcbfbee550e
Daniel Coroama <daniel.coroama21@imperial.ac.uk>
2024-03-01 17:44:20 +0000

    Finished builder helpers

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -7,27 +7,22 @@
 open DrawModelType.SymbolT
 open CommonTypes
 open Optics
+open Symbol
 
 // B1R
 let getCustomComponentDimensions (sym: Symbol) : float * float = 
     // Use of lens already provided for getting bounding box
-    match sym.Component.Type with
-    | Custom _ ->
-        let box = sym.LabelBoundingBox
-        box.W, box.H
-    | _ -> failwithf "Non-custom component pattern matching with get"
+    let comp = sym.Component
+    comp.W, comp.H
 
 // B1W
 let setCustomComponentDimensions 
-    (newDim: float * float) // This is length in x by length in y
+    (newDim: float * float) // This is width by height
     (sym: Symbol) 
     : Symbol =
-    match sym.Component.Type with
-    | Custom _ ->
-        let box = sym.LabelBoundingBox
-        let newBox = {box with W = fst newDim; H = snd newDim}
-        snd labelBoundingBox_ newBox sym // Using provided lenses
-    | _ -> failwithf "Non-custom component pattern matching with set"
+    let comp = sym.Component
+    let newComp = {comp with W = fst newDim; H = snd newDim}
+    snd component_ newComp sym // Using provided lenses
 
 // B1 Lens
 let customComponentDimension_ = Lens.create getCustomComponentDimensions setCustomComponentDimensions
@@ -37,20 +32,71 @@ let setSymbolPos (sym: Symbol) (pos: XYPos) =
     {sym with Pos=pos}
 
 // B3R
-// Read port order on specified side of a symbol.
+/// Read port order on specified side of a symbol.
 let getPortOrder (side: Edge) (sym: Symbol) =
     sym.PortMaps.Order[side]
 
 // B3W
-// Write port order on a specified side of a symbol
+/// Write port order on a specified side of a symbol
 let setPortOrder (side: Edge) (order: string list) (sym: Symbol) =
-    let oldPortOrder = sym.PortMaps.Order
-    let newPortOrder = Map.add side order oldPortOrder
-    {sym with PortMaps={sym.PortMaps with Order=newPortOrder}}
+    sym.PortMaps.Order
+    |> Map.add side order
+    // {sym with PortMaps={sym.PortMaps with Order=newPortOrder}}
+    |> fun newPortOrder -> snd portMaps_ {sym.PortMaps with Order=newPortOrder} sym
 
 // B3 Lens
 let changePortOrder_ (edge: Edge) = Lens.create (getPortOrder edge) (setPortOrder edge)
 
-// B4R -- Don't understand requirement
+// B4R
+/// Get the reverses state of the inputs of a MUX2
+let getReverseStateMux (sym: Symbol) =
+    sym.ReversedInputPorts
+
+// B4W
+/// Set the reverses state of the inputs of a MUX2
+let setReverseStateMux (newReverse: bool option) (sym: Symbol) =
+    {sym with ReversedInputPorts=newReverse}
+
+// B4 Lens
+let reverseStateMux_ = Lens.create getReverseStateMux setReverseStateMux
 
 // B5R
+/// Read the position of a port on the sheet
+let getPortPosition (sheet: Model) (port: Port) =
+    let sym = sheet.Symbols[ComponentId port.HostId]
+    let offset = getPortPos sym port
+    sym.Pos + offset
+
+// B6R
+/// Get the bounding box of a symbol outline
+let getBoundingBox (sym: Symbol) =
+    getSymbolBoundingBox sym
+
+// B7R
+/// Get the rotation state of a symbol
+let getRotationState (sym: Symbol) =
+    sym.STransform.Rotation
+
+// B7W
+let setRotationState (newRotation: Rotation) (sym: Symbol) =
+    {sym with STransform={sym.STransform with Rotation=newRotation}}
+
+// B7 Lens
+let rotationState_ = Lens.create getRotationState setRotationState
+
+// B8R
+/// Get the rotation state of a symbol
+let getFlipState (sym: Symbol) =
+    sym.STransform.Flipped
+
+// B8W
+let setFlipState (newFlip: bool) (sym: Symbol) =
+    {sym with STransform={sym.STransform with Flipped=newFlip}}
+
+// B8 Lens
+let flipState_ = Lens.create getFlipState setFlipState
+
+
+//------------ Testing Helpers ------------//
+
+// T1R
